d30e3a7440fff8806d5f6b7aa914d3770438ed5d
c-f-h <chofreither@gmail.com>
2014-01-24 01:16:39 +0200

    Revert previous commit for now, leads to crashes.
    This reverts commit d510c8c12666ffcd8acc580b49082ff8d6ac159b.

--- pin/player.cpp
+++ pin/player.cpp
@@ -846,7 +846,7 @@ HRESULT Player::Init(PinTable * const ptable, const HWND hwndProgress, const HWN
 
 			ph->GetTimers(&m_vht);
 
-			//if (g_pvp->m_pdd.m_fHardwareAccel)
+    		if (g_pvp->m_pdd.m_fHardwareAccel)
 			{
   				if ((m_ptable->m_vedit.ElementAt(i)->GetItemType() == eItemRamp && ((Ramp*)m_ptable->m_vedit.ElementAt(i))->m_d.m_fAlpha) ||
 	  				(m_ptable->m_vedit.ElementAt(i)->GetItemType() == eItemPrimitive && !((Primitive *)m_ptable->m_vedit.ElementAt(i))->m_d.staticRendering) ||
@@ -855,11 +855,11 @@ HRESULT Player::Init(PinTable * const ptable, const HWND hwndProgress, const HWN
 					  m_vhitalpha.AddElement(ph);
 					}
 			}
-			//else
-			//	if (m_ptable->m_vedit.ElementAt(i)->GetItemType() == eItemPrimitive)
-			//		{
-			//		  m_vhitalpha.AddElement(ph);
-			//		}
+			else
+				if (m_ptable->m_vedit.ElementAt(i)->GetItemType() == eItemPrimitive)
+					{
+					  m_vhitalpha.AddElement(ph);
+					}
 		}
 	}
 
