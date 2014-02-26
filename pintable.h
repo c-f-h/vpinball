9e6f68b507beb2922b05a6854674c81adf4afae0
toxieainc <toxie@ainc.de>
2014-02-26 10:03:54 +0100

    kick out alternate render from UI

--- pintable.h
+++ pintable.h
@@ -251,11 +251,11 @@ public:
 	STDMETHOD(get_GlobalDifficulty)(/*[out, retval]*/ float *pVal);
 	STDMETHOD(put_GlobalDifficulty)(/*[in]*/ float newVal);
 
+	//!! deprecated
 	STDMETHOD(get_HardwareRender)(/*[out, retval]*/ VARIANT_BOOL *pVal);
 	STDMETHOD(put_HardwareRender)(/*[in]*/ VARIANT_BOOL newVal);
 	STDMETHOD(get_AlternateRender)(/*[out, retval]*/ VARIANT_BOOL *pVal);
 	STDMETHOD(put_AlternateRender)(/*[in]*/ VARIANT_BOOL newVal);
-	/////////////////////////////////////////////
 
 	STDMETHOD(get_DeadSlider)(/*[out, retval]*/  int *pVal);
 	STDMETHOD(put_DeadSlider)(/*[in]*/ int newVal);
